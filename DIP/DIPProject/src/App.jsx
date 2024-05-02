681db92d9d56c15fd793e6e0e20fe888e91480f6
Ishaan-Chandak <103961770+Ishaan-Chandak@users.noreply.github.com>
2024-05-02 19:06:45 +0530

    initial commit

---
+++ DIP/DIPProject/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import Navbar from './Components/Navbar'
+import Footer from './Components/Footer'
+import Form from './Components/Form'
+
+function App() {
+
+  return (
+    <>
+      <Navbar />
+
+      <Form />
+
+      <div class='fixed bottom-0 w-full'>
+        <Footer />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ DIP/DIPProject/src/Components/Form.jsx
@@ -0,0 +1,140 @@
+import React, { useState, useRef } from 'react'
+import Dropzone from "react-dropzone";
+import { AiOutlineCloudUpload } from "react-icons/ai";
+import axios from "axios";
+
+const Form = () => {
+    const file = useRef(null)
+    const [diag, setDiag] = useState(null);
+    const [filename, setfilename] = useState(null)
+    const [symptomsImageURL, setSymptomsImageURL] = useState('');
+
+    const handleSubmit = async () => {
+        if (file.current) {
+            const cloud = new FormData()
+            cloud.append("file", file.current)
+            cloud.append("upload_preset", "dip_project")
+            cloud.append("cloud_name", "djzejdmyb")
+
+            const response1 = await fetch(
+                "https://api.cloudinary.com/v1_1/djzejdmyb/image/upload",
+                {
+                    method: "POST",
+                    body: cloud,
+                }
+            )
+
+            console.log(response1);
+            const data1 = await response1.json()
+
+            console.log("sent to cloudinary");
+            console.log(data1);
+
+            try {
+                const form = new FormData()
+                form.append("file", file.current)
+                const response = await axios.post("http://localhost:5000/predict", form)
+                    .then((res) => {
+                        setDiag(res.data.class)
+                    })
+            }
+            catch (err) {
+                console.log(err)
+            }
+        }
+    }
+
+    const handlefiles = async (param) => {
+        if (param[0]) {
+            const type = param[0].type
+            const splitted = type.split("/")
+
+            let temp1 = "jpeg"
+            let temp2 = "png"
+
+            if (splitted[1] === temp1 || splitted[1] === temp2) {
+                file.current = param[0]
+                const myFile = new FileReader()
+                myFile.addEventListener("load", () => {
+                    console.log(myFile.result);
+                    setSymptomsImageURL(myFile.result)
+                }, false)
+                myFile.readAsDataURL(file.current)
+                // setError(null)
+            } else {
+                // setError("Please upload supported files only")
+                setfilename(null)
+                file.current = null
+            }
+        } else {
+            // setError("Please upload single file only")
+            setfilename(null)
+            file.current = null
+        }
+
+        // setTimeout(() => {
+        //   setError(null)
+        // }, 4000)
+    }
+    return (
+        <div class="p-2">
+            <Dropzone
+                className='dropzone'
+                onDrop={(acceptedFiles) => handlefiles(acceptedFiles)}
+                multiple={false}
+            >
+                {({ getRootProps, getInputProps }) => (
+                    <section>
+                        <div {...getRootProps()}>
+                            <input {...getInputProps()} />
+                            <div style={{ display: 'flex', flexDirection: 'column' }}>
+                                <p style={{ textAlign: 'center' }}>
+                                    Drag and drop your image here, or click to select a
+                                    file
+                                </p>
+                                <p style={{ textAlign: 'center' }}>
+                                    Accepts JPEG and PNG formats, limited to one image at
+                                    a time.
+                                </p>
+                                <AiOutlineCloudUpload
+                                    style={{
+                                        transform: 'scale(3)',
+                                        marginTop: '3vh',
+                                        cursor: 'pointer',
+                                        alignSelf: 'center',
+                                    }}
+                                />
+                            </div>
+                        </div>
+                    </section>
+                )}
+            </Dropzone>
+
+
+            <div class="p-5">
+                {symptomsImageURL ? (<img src={symptomsImageURL} />) : <p> No file uploaded </p>}
+            </div>
+
+            {
+                !diag ? (<div>
+                    <p style={{ fontWeight: '700', textAlign: 'center', fontSize: 'large' }}>Discover your path to better health. Take the first step with our model's precise answers.</p>
+
+                    <p style={{ fontWeight: '700', textAlign: 'center', fontSize: 'large' }}>
+                        <button
+
+                            class="bg-blue-500 text-white p-2 border-none outline-none rounded-2xl font-bold text-lg shadow-md cursor-pointer"
+                            onClick={handleSubmit}
+                        >
+                            Send for diagnosis
+                        </button>
+                    </p>
+                </div>)
+                    : (
+                        diag
+                    )
+            }
+        </div >
+    )
+}
+
+export default Form
